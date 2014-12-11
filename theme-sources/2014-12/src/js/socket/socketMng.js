24d903c472f3df305cd4c0118979ad6d27e421c5
nulltask <nulltask@gmail.com>
2014-12-11 16:04:32 +0900

    write up socket manger

--- theme-sources/2014-12/src/js/socket/socketMng.js
+++ theme-sources/2014-12/src/js/socket/socketMng.js
@@ -1,15 +1,47 @@
-var Socket_client = require('socket.io-client');
+
+var io = require('socket.io-client');
+
+module.exports = SocketMng;
 
 function SocketMng(emitter){
   this.emitter = emitter;
-  this.socket_client = new Socket_client();
+  this.socket = io('localhost:3000');
+  this.tweets = [];
+
+  this.socket.on('set', this.onset.bind(this));
+  this.socket.on('reset', this.onreset.bind(this));
+  this.socket.on('start', this.onstart.bind(this));
+  this.socket.on('pause', this.onpause.bind(this));
+  this.socket.on('update', this.onupdate.bind(this));
+  this.socket.on('track', this.ontrack.bind(this));
+  this.socket.on('tweet', this.ontweet.bind(this));
 }
 
-SocketMng.prototype.setSocketEvents = function(){
-  // emit on sockets
-  this.socket_client.on('',function(){
+SocketMng.prototype.onset = function(sec) {
+  this.emitter.emit('set', sec);
+};
 
-  });
-}
+SocketMng.prototype.onreset = function() {
+  this.emitter.emit('reset');
+};
 
-module.exports = SocketMng;
+SocketMng.prototype.onstart = function() {
+  this.emitter.emit('start');
+};
+
+SocketMng.prototype.onpause = function() {
+  this.emitter.emit('pause');
+};
+
+SocketMng.prototype.ontrack = function(query) {
+  this.emitter.emit('track', query);
+};
+
+SocketMng.prototype.onupdate = function(name, title) {
+  this.emitter.emit('update', name, title);
+};
+
+SocketMng.prototype.ontweet = function(tweet) {
+  this.tweets.push(tweet);
+  this.emitter.emit('tweet', tweet);
+};
