3b57647b6a483ac6dba0b1129a6a4b3c6c7beff1
monpy <4696monpy@gmail.com>
2014-12-11 17:15:27 +0900

    transition の設定。

--- theme-sources/2014-12/src/js/views/View.js
+++ theme-sources/2014-12/src/js/views/View.js
@@ -9,6 +9,14 @@ function View(emitter){
   //sec
   this.speakTime = 600;
 
+  this.emitter.on('set', this.set.bind(this));
+  this.emitter.on('reset', this.reset.bind(this));
+  this.emitter.on('start', this.start.bind(this));
+  this.emitter.on('pause', this.pause.bind(this));
+  this.emitter.on('update', this.update.bind(this));
+  this.emitter.on('track', this.track.bind(this));
+  this.emitter.on('tweet', this.tweet.bind(this));
+
 };
 
 View.prototype.insertAudience = function(){
@@ -21,18 +29,11 @@ View.prototype.insertAudience = function(){
 
 View.prototype.setEmitterEvents = function(){
   //recieve event from socketMng
-  this.emitter.on('set', this.set.bind(this));
-  this.emitter.on('reset', this.reset.bind(this));
-  this.emitter.on('start', this.start.bind(this));
-  this.emitter.on('pause', this.pause.bind(this));
-  this.emitter.on('update', this.update.bind(this));
-  this.emitter.on('track', this.track.bind(this));
-  this.emitter.on('tweet', this.tweet.bind(this));
 };
 
 View.prototype.update = function(speakerName, speakerTitle){
-  this.$('.lt_speaker__name').text(fullPitchString(speakerName));
-  this.$('#speaker_title').text(speakerTitle + "亭");
+  this.$('.lt_speaker__name').text(fullPitchString(speakerName) + "亭");
+  this.$('#speaker_title').text(speakerTitle);
 };
 
 
@@ -41,21 +42,37 @@ View.prototype.reset = function(){
 };
 
 View.prototype.set = function(sec){
+  console.log(sec);
   this.speakTime = sec;
 };
 
 View.prototype.start = function(sec){
+  console.log('start!!!');
   this.speakTime = sec;
+
+  // first position = 52%
+  var basePos = 0;
+  this.$('#close_left').css({left: -basePos'%'});
+  this.$('#close_right').css({right: -basePos'%'});
 };
 
 View.prototype.tick = function(until){
-
+  var per = this.speakTime / until;
+  this.$('#close_left').css({left:-per + '%'});
+  this.$('#close_right').css({right:-per + '%'});
 };
 
-View.prototype.tweet = function(){
+View.prototype.tweet = function(tweet){
+  console.log(tweet);
+};
 
-}
+View.prototype.track = function(){
+  //検索キーワードがでてきたとき,,,
+};
 
+View.prototype.pause = function(){
+
+};
 
 
 module.exports = View;
