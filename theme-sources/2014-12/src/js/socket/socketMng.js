991e38b20213f73218e48092222a5e766e4b7d82
monpy <4696monpy@gmail.com>
2014-12-11 20:08:12 +0900

    tweet 流し込みを実装

--- theme-sources/2014-12/src/js/socket/socketMng.js
+++ theme-sources/2014-12/src/js/socket/socketMng.js
@@ -18,30 +18,37 @@ function SocketMng(emitter){
 }
 
 SocketMng.prototype.onset = function(sec) {
+  console.log('onset');
   this.emitter.emit('set', sec);
 };
 
 SocketMng.prototype.onreset = function() {
+  console.log('onreset');
   this.emitter.emit('reset');
 };
 
 SocketMng.prototype.onstart = function() {
+  console.log('onstart');
   this.emitter.emit('start');
 };
 
 SocketMng.prototype.onpause = function() {
+  console.log('onpause');
   this.emitter.emit('pause');
 };
 
 SocketMng.prototype.ontrack = function(query) {
+  console.log('ontrack');
   this.emitter.emit('track', query);
 };
 
 SocketMng.prototype.onupdate = function(name, title) {
+  console.log('onupdate');
   this.emitter.emit('update', name, title);
 };
 
 SocketMng.prototype.ontweet = function(tweet) {
+  console.log('ontweet');
   this.tweets.push(tweet);
   this.emitter.emit('tweet', tweet);
 };
